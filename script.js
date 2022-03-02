eee8fac191f28d229542c8d739718d5b84d6fcfa
Farhan-Abid <shahabid628@gmail.com>
2022-03-02 16:40:29 +0600

    changed everything

--- script.js
+++ script.js
@@ -15,13 +15,12 @@ const searchMobile = () => {
 const displaySearchResult = phones => {
   console.log(phones);
   const searchResult = document.getElementById('search-result');
-  const mobileSearchResult = document.getElementById('mobile-search-result');
-  const h2 = document.createElement('h2');
+  searchResult.textContent = '';
   if(phones.status===false){
-    
-    h2.classList.add('text-center');
-    h2.innerText = "Your search result is not available at the moment.";
-    mobileSearchResult.appendChild(h2);
+  const div = document.createElement('h2');
+    div.classList.add('text-center');
+    div.innerHTML = `<h2>Your search result is not available at the moment</h2>`;
+    searchResult.appendChild(div);
     }
     
   phones.data.slice(0,20).map(datas => {
@@ -40,8 +39,9 @@ const displaySearchResult = phones => {
       searchResult.appendChild(div);
 
   })
+  
 }
-h2.textContent = '';
+
 
 const loadMobileDetail = mobileId => {
   console.log(mobileId);
@@ -74,12 +74,12 @@ const displayMobileDetail = data => {
             <p class="card-text">${data.mainFeatures.sensors[4]}</p>
             <p class="card-text">${data.mainFeatures.sensors[5]}</p>
             <h5>Others:</h5>
-            <p>Wlan: ${data.others.WLAN ? data.others.WLAN: 'not available'}</p>
-            <p>Blurtooth: ${data.others.Bluetooth ? data.others.Bluetooth: 'not available'}</p>
-            <p>GPS: ${data.others.GPS ? data.others.GPS: 'not available'}</p>
-            <p>NFC: ${data.others.NFC ? data.others.NFC: 'not available'}</p>
-            <p>Radio: ${data.others.Radio ? data.others.Radio: 'not available'}</p>
-            <p>USB: ${data.others.USB ? data.others.USB: 'not available'}</p>
+            <p>Wlan: ${data?.others?.WLAN ? data?.others?.WLAN: 'not available'}</p>
+            <p>Blurtooth: ${data?.others?.Bluetooth ? data?.others?.Bluetooth: 'not available'}</p>
+            <p>GPS: ${data?.others?.GPS ? data?.others?.GPS: 'not available'}</p>
+            <p>NFC: ${data?.others?.NFC ? data?.others?.NFC: 'not available'}</p>
+            <p>Radio: ${data?.others?.Radio ? data?.others?.Radio: 'not available'}</p>
+            <p>USB: ${data?.others?.USB ? data?.others?.USB: 'not available'}</p>
           </div>
   `;
   mobileDetail.appendChild(div);
